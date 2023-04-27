24ac099d4d1640acf74e4c480d97b0cf6c43ef27
Vishal Kaushik <vishalkaushik2002@gmail.com>
2023-04-27 10:05:19 +0530

    Updated the sync mechanism

--- i_18n_app/views.py
+++ i_18n_app/views.py
@@ -1,3 +1,4 @@
+import json
 import os
 from django.shortcuts import render
 from .models import Article
@@ -7,6 +8,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from i_18n_app.serializers import ArticleListSerializer
 
+
 # Create your views here.
 def get_existing_languages(request):
     articles = Article.objects.all()
@@ -14,13 +16,12 @@ def get_existing_languages(request):
     articles = article_serializer.data
     return JsonResponse({"Response": articles}, status=status.HTTP_200_OK)
 
+
 def sync_languages_to_db(request):
-    articles = Article.objects.all()
-    article_serializer = ArticleListSerializer(articles, many=True)
-    articles = article_serializer.data
-    languages = list(settings.LANGUAGES)
-    for language in languages:
-        language_code, language_name = language
-        new_language_obj_to_be_added_to_db = Article(title={language_code: str(language_name)})
-        new_language_obj_to_be_added_to_db.save()
-    return JsonResponse({"Response": articles}, status=status.HTTP_200_OK)
+    article = Article.objects.get(id=2)
+    languages_dict = {code: str(name) for code, name in settings.LANGUAGES}
+    languages_json = json.dumps(languages_dict)
+    print(f"{languages_json = }")
+    article.title = languages_json
+    article.save()
+    return JsonResponse({"Hello": "World"}, status=status.HTTP_200_OK)
