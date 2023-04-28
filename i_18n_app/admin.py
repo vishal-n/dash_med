226ee82e44c9bbd4b6d72aca06d5af3afd8e0ad2
Vishal Kaushik <vishalkaushik2002@gmail.com>
2023-04-28 22:56:31 +0530

    Article admin override

--- i_18n_app/admin.py
+++ i_18n_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from i_18n_app.models import Article
+# from i_18n_app import ArticleAdmin
 
 # Register your models here.
-admin.site.register(Article)
+# admin.site.register(Article, ArticleAdmin)
--- i_18n_app/models.py
+++ i_18n_app/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.contrib import admin
 from django import forms
+from django.core.serializers.json import DjangoJSONEncoder
 from .forms import JSONFormField
 
+class CustomJSONEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        # Add custom JSON encoding logic here
+        return super().default(obj)
+
 class Article(models.Model):
-    title = models.JSONField("Article Title", default=dict)
+    title = models.JSONField("Article Title", default=dict, encoder=CustomJSONEncoder)
 
     def __str__(self) -> str:
         return self.title
 
 
-# class ArticleAdmin(admin.ModelAdmin):
-#     formfield_overrides = {
-#         models.JSONField: {'form_class': JSONFormField},
-#     }
+class ArticleAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.JSONField: {'form_class': JSONFormField},
+    }
 
-# admin.site.register(Article, ArticleAdmin)
+# admin.site.register(ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
